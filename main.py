798e9637ba027e24c93483f11efa7afb8a3f6bb8
jeay <wrjie@msn.cn>
2018-10-31 16:41:02 +0800

    -h
    Signed-off-by: jeay <wrjie@msn.cn>

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+#!python
+# -*- coding: UTF-8 -*-
+
+'''
+/*
+ * @Author: jeay
+ * @Date: 2018-10-31 16:30:12
+ * @Last Modified by: jeay
+ * @Last Modified time: 2018-10-31 16:33:23
+ */
+'''
+
 import time
 from git import Repo, Actor
+# pip install gitpython
 
 author = Actor("jeay", "wrjie@msn.cn")
 committer = Actor("Auto Git", "wrjie@msn.cn")
-# commit by commit message and author and committer
 
-def main():
-    repo = Repo('/home/w/git/python/gittest')
+dirList = (
+    '/rsync/lmlq',
+    '/rsync/kefid',
+    '/rsync/bd'
+)
+
+def main(dirPath):
+    repo = Repo(dirPath)
     if repo.is_dirty():
         index = repo.index
         index.add(['*'])
         index.commit('back up at ' + time.strftime('%Y-%m-%d',time.localtime(time.time())), author=author, committer=committer)
-        print('commit')
+        print(dirPath + ' has committed')
     else:
-        print('is clean')
-
+        print(dirPath + ' is clean')
 
 if __name__ == '__main__':
-    main()
+    for dirPath in dirList:
+        main(dirPath)
